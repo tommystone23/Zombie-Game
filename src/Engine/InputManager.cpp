b81b6ba98993072d9314044e910701e9172b4897
Tommy <tommy.stone23@outlook.com>
2023-06-18 11:31:39 -0400

    Updated Engine code with updates to InputManager

--- src/Engine/InputManager.cpp
+++ src/Engine/InputManager.cpp
@@ -8,6 +8,13 @@ InputManager::~InputManager()
 {
 }
 
+void InputManager::update()
+{
+    for(auto &it : _key_map) {
+        _previous_key_map[it.first] = it.second;
+    }
+}
+
 void InputManager::press_key(unsigned int key_id)
 {
     _key_map[key_id] = true;
@@ -18,11 +25,32 @@ void InputManager::release_key(unsigned int key_id)
     _key_map[key_id] = false;
 }
 
-bool InputManager::is_key_pressed(unsigned int key_id)
+// Returns true if key is held down
+bool InputManager::is_key_down(unsigned int key_id)
 {
     if(_key_map.find(key_id) != _key_map.end())
     {
         return _key_map[key_id];
+    }
+    return false;
+}
+
+// Returns true if key was just pressed
+bool InputManager::is_key_pressed(unsigned int key_id)
+{
+    // Check if it was pressed this frame and not last frame
+    bool is_pressed;
+    if(is_key_down(key_id) && !was_key_down(key_id))
+        return true;
+
+    return false;
+}
+
+bool InputManager::was_key_down(unsigned int key_id)
+{
+    if(_previous_key_map.find(key_id) != _previous_key_map.end())
+    {
+        return _previous_key_map[key_id];
     }
     return false;
 }
--- src/Engine/InputManager.h
+++ src/Engine/InputManager.h
@@ -1,3 +1,10 @@
+/*
+    Handles all player input. key_id parameters are SDL id's that
+    will be accessible every frame. 
+    Functions press_key, release_key, and set_mouse_coords should be called 
+    in the SDLPollEvent loop. The other functions can be called by other
+    game elements to check what buttons have been pressed that frame.
+*/
 #ifndef INPUT_MANAGER_H
 #define INPUT_MANAGER_H
 #include <unordered_map>
@@ -9,17 +16,23 @@ public:
     InputManager();
     ~InputManager();
 
+    void update();
+
     void press_key(unsigned int key_id);
     void release_key(unsigned int key_id);
 
+    bool is_key_down(unsigned int key_id);
     bool is_key_pressed(unsigned int key_id);
 
     void set_mouse_coords(float x, float y);
     glm::vec2 get_mouse_coords() const { return _mouse_coords; }
 private: 
     std::unordered_map<unsigned int, bool> _key_map;
+    std::unordered_map<unsigned int, bool> _previous_key_map;
 
     glm::vec2 _mouse_coords;
+
+    bool was_key_down(unsigned int key_id);
 };
 
 #endif
